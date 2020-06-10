5d97b11c60a9d32c63320d53df4c8501fd1f3605
David-invictus <65469731+David-invictus@users.noreply.github.com>
2020-06-10 15:01:44 -0300

    Update formulario.php

--- ProgWeb_At-Online_III/formulario.php
+++ ProgWeb_At-Online_III/formulario.php
@@ -1,32 +1,3 @@
-<?php
-/*
- * formulario.php
- * 
- * Copyright 2020  <pi@raspberrypi>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- * 
- * Atividade Proposta:
- * Receber os dados enviados no formulário da página pessoal em PHP e armazenar os mesmos em variáveis,
- * construindo uma segunda página em PHP, que mostra os dados enviados.
- * Por fim, como pontuação extra, pesquise sobre o heroku, submeta a sua página pessoal nessa plataforma
- * (heroku) e disponibilize para os colegas.
- */
-?>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pt-br" lang="pt-br" xml:lang="en" lang="en">
